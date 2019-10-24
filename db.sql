6083cdb52c918ce12b4d57cb176cd43107614bf8
dbond762 <dbond762@gmail.com>
2019-10-24 17:05:12 +0300

    create db

---
+++ db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE `ci_menu`.`user` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `name` VARCHAR(50) NOT NULL ,
+  `password` VARCHAR(50) NOT NULL ,
+  `is_admin` BOOLEAN NOT NULL DEFAULT FALSE ,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE `ci_menu`.`menu` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `label` VARCHAR(50) NOT NULL ,
+  `link` VARCHAR(100) NOT NULL ,
+  `order` INT(3) NOT NULL ,
+  `is_draft` BOOLEAN NOT NULL DEFAULT FALSE ,
+  `parrent` INT(11) NULL ,
+  PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+ALTER TABLE `menu` ADD CONSTRAINT `menu_parrent` FOREIGN KEY (`parrent`) REFERENCES `user`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
